26a88d32ad15b19c9fc50092fe28a3d4bc27e704
D21125387 <D21125387@mytudublin.ie>
2022-03-21 18:08:27 +0000

    Lab7

---
+++ Lab7/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    echo "Hello World!<br/>";
+
+    $name = "Chakkarin";
+    echo strlen($name)."<br/>";
+
+    $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+*/
+
+    function multiply($x, $y){
+        return $x*$y;
+    }
+
+    echo multiply($_POST["x"],$_POST["y"]);
